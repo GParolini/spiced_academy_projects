926c347304fc0a550fddef412e378183875679f9
Giuditta <giudittaparolini@gmail.com>
2021-04-24 12:26:26 +0200

    presentation week6

--- project6_pipeline/slackbot_bvg/slackbot_bvg.py
+++ project6_pipeline/slackbot_bvg/slackbot_bvg.py
@@ -10,20 +10,20 @@ conn_str = 'postgresql://postgres:1234@my_postgres:5432/my_bvg_data'
 engine = create_engine(conn_str, echo=True)
 
 query_late='''
-    SELECT running_time, delay, type, name, direction
+    SELECT running_time, delay, name
     FROM s_koepenick_timetable
-    WHERE delay >60
+    WHERE delay > 60
     ORDER BY running_time DESC
 '''
 
 late= pd.read_sql(query_late, engine).drop_duplicates()
 late['running_time']=pd.to_datetime(late['running_time'])
 
-text1= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=(round(int(late.at[0, 'delay'])/60)), name=late.at[0, 'name'], my_time=str(late.at[0, 'running_time'].hour)+":"+str(late.at[0, 'running_time'].minute), my_day=str(late.at[0, 'running_time'].day)+" "+ str(late.at[0, 'running_time'].strftime("%B")))
+text1= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=round(int(late.at[0, 'delay'])/60), name=late.at[0, 'name'], my_time=str(late.at[0, 'running_time'].hour)+":"+str(late.at[0, 'running_time'].minute), my_day=str(late.at[0, 'running_time'].day)+" "+ str(late.at[0, 'running_time'].strftime("%B")))
 
-text2= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=(round(int(late.at[1, 'delay'])/60)), name=late.at[1, 'name'], my_time=str(late.at[1, 'running_time'].hour)+":"+str(late.at[1, 'running_time'].minute), my_day=str(late.at[1, 'running_time'].day)+" "+ str(late.at[1, 'running_time'].strftime("%B")))
+text2= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=round(int(late.at[1, 'delay'])/60), name=late.at[1, 'name'], my_time=str(late.at[1, 'running_time'].hour)+":"+str(late.at[1, 'running_time'].minute), my_day=str(late.at[1, 'running_time'].day)+" "+ str(late.at[1, 'running_time'].strftime("%B")))
 
-text3= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=(round(int(late.at[2, 'delay'])/60)), name=late.at[2, 'name'], my_time=str(late.at[2, 'running_time'].hour)+":"+str(late.at[2, 'running_time'].minute), my_day=str(late.at[2, 'running_time'].day)+" "+ str(late.at[2, 'running_time'].strftime("%B")))
+text3= "There will be a delay of {delay} minutes on the line {name} from S-Bahn Köpenick at {my_time} on {my_day}.".format(delay=round(int(late.at[2, 'delay'])/60), name=late.at[2, 'name'], my_time=str(late.at[2, 'running_time'].hour)+":"+str(late.at[2, 'running_time'].minute), my_day=str(late.at[2, 'running_time'].day)+" "+ str(late.at[2, 'running_time'].strftime("%B")))
 
 
 WEBHOOK_URL = config.WEBHOOK_URL
